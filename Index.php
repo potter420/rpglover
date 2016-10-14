bde020e81d732a8396dd6f43fc62ce1e02e33324
potter420 <thanhmt0612@gmail.com>
2016-10-14 11:34:56 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,6 +4,20 @@
 <link rel="stylesheet" type="text/css" href="css/default.css">
 </head>
 <body>
+<ul class = "headbar">
+<li class = "headbar"><a class="nest1" href="#home" class="Homebtt">RPG LOVER</a></li>
+<li class = "headbar"><a class="nest1"href="#list">RPG List</a></li>
+<li class = "headbar"><a class="nest1"href="#about">About</a></li>
+<li class = "headbar"><a class="nest1"href="#contact">Contact Us!</a></li>
+<li class = "headbar">
+<div>Search
+<ul>
+<li><a class="nest2"href="#byname">By name:</a></li>
+<li><a class="nest2"href="#bycate">By category:</a></li>
+</ul>
+</div>
+</li>
+</ul>
 <p class="title" >Some Title Here</p>
 <p class="text">Here go something about what we do </p>
 <?php
